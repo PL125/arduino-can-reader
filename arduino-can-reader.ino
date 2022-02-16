65d4b99896393ab33b6dc1d314ee30452e92111c
Pedro L <pedroloureiro1@sapo.pt>
2022-02-16 11:23:13 +0000

    Addapt to work with Opel Mid Speed can-bus
    Force silent mode to be sure we don't interfere with anything else on the car

--- arduino-can-reader.ino
+++ arduino-can-reader.ino
@@ -1,5 +1,14 @@
 #include <SPI.h>
-#include "mcp_can.h"
+#include <mcp2515_can.h>    // https://github.com/Seeed-Studio/Seeed_Arduino_CAN
+
+
+/*///////////////////////////////////////////////////////
+  Opel/Vauxhall configs
+    HSCAN - Dual-wire,   High Speed CAN-BUS,   500 kbps
+    MSCAN - Dual-wire,   Medium Speed CAN-BUS, 95 kbps
+    SWCAN - Single-wire, Low Speed CAN-BUS,    33.3 kbps
+*////////////////////////////////////////////////////////
+
 
 ///////////////////
 // Configuration //
@@ -14,22 +23,20 @@ const int CS_PIN = 9;
 // Serial port data rate
 const long SERIAL_SPEED = 115200;
 
-// CAN bus data rate
-const byte CAN_SPEED = CAN_125KBPS;
-
 ///////////////////
 
-MCP_CAN CAN(CS_PIN);
+mcp2515_can CAN(CS_PIN);    // Set CS pin
 unsigned char len = 0;
 byte buffer[8];
 
 void setup() {
     Serial.begin(SERIAL_SPEED);
-    byte canSpeed = CAN_SPEED;
 
-    while (CAN.begin(canSpeed) != CAN_OK) {
+    while (CAN.begin(CAN_95KBPS, MCP_16MHz) != CAN_OK) {
       delay(100);
     }
+
+    CAN.setMode(MODE_LISTENONLY);  // Don't send the ACK bit, this may disturb or change the behavior of the system we are trying to sniff (silent mode)
 }
 
 void loop() {
@@ -40,13 +47,13 @@ void loop() {
         Serial.print(CAN.getCanId());
         Serial.print(":LEN=");
         Serial.print(len);
-        
+
         char tmp[3];
         for(int i = 0; i<len; i++) {
             Serial.print(":");
-            
+
             snprintf(tmp, 3, "%02X", buffer[i]);
-            
+
             Serial.print(tmp);
         }
 
